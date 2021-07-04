afdafae4920a9d037a91f7fac677232df6b80d30
Shon-Elias <shon.elias@gmail.com>
2021-07-04 20:35:56 +0300

    Posting new post

--- src/Components/AddComment.js
+++ src/Components/AddComment.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
-
+import axios from 'axios';
+import { nanoid } from 'nanoid';
 
 const AddComment = () => {
 
@@ -8,7 +9,30 @@ const AddComment = () => {
   const submitComment = (e) => {
     e.preventDefault()
 
+    const comment = {
+      id: 1,
+      postId: nanoid(),
+      email: 'example@example.com',
+      name: 'username',
+      body: newComment
+    }
+
+    postComment(comment);
   }
+
+  const postComment = comment => {
+    axios.post('https://jsonplaceholder.typicode.com/posts', comment
+    )
+    .then(res => {
+      console.log("post", res);
+
+      setNewComment('')
+    })
+    .catch(e => {
+      console.log('post req err', e)
+    })
+  }
+
 
 
 
