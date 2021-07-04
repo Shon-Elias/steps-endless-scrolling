52b253896d38b3b7b64d3243039bac4308ab1444
Shon-Elias <shon.elias@gmail.com>
2021-07-04 20:30:36 +0300

    add comment basic form

--- src/Components/AddComment.js
+++ src/Components/AddComment.js
@@ -1,6 +1,37 @@
+import { useState } from "react"
+
+
 const AddComment = () => {
+
+  const [newComment, setNewComment] = useState('')
+
+  const submitComment = (e) => {
+    e.preventDefault()
+
+  }
+
+
+
+
   return (
-    <div className='add-comment-box'>AddComment</div>
+    <form  onSubmit={submitComment} >
+        <div  >
+        <div>
+          <textarea
+            placeholder=" comment"
+            value={newComment}
+            onChange={e=>setNewComment(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="submit"
+            value="Post" />
+        </div>
+        </div>
+    </form>
   )
 }
+
 export default AddComment;
